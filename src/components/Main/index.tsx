f878482fb51f3c0c89f8620aa8ecb1239c569a05
Marcio Lisboa <mjlbnu@gmail.com>
2021-03-17 22:10:08 -0300

    Files add

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,14 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/rlogo.png" alt="logo" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src="/prog.svg" alt="" />
+  </S.Wrapper>
 )
 
 export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+// import App from "next/app";
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="short icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content=" A simple project to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
